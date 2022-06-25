ab261890235e056bf1c6f07e0a2ce61bdbee2c66
Bruno Augusto Claro <brunaugusto@gmail.com>
2022-06-25 19:06:28 -0300

    Begin

--- src/modules/clients/useCases/createClient/CreateClientUseCase.ts
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.ts
@@ -6,6 +6,8 @@ interface ICreateClient {
     username: string;
     password: string;
 }
+
+//aqui
 export class CreateClientUseCase {
     async execute({ username, password } : ICreateClient): Promise<any>{
         //validar se o client existe
