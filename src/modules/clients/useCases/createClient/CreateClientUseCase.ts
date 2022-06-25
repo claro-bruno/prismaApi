d9e0f2fca984faf79a130fd56ac2000946ef1b0e
Bruno Augusto Claro <brunaugusto@gmail.com>
2022-06-25 18:37:31 -0300

    Repo

--- src/modules/clients/useCases/createClient/CreateClientUseCase.ts
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.ts
@@ -6,6 +6,7 @@ interface ICreateClient {
     username: string;
     password: string;
 }
+//aqui
 
 export class CreateClientUseCase {
     async execute({ username, password } : ICreateClient): Promise<any>{
